eb4814c63b9da0f616025417dd77759a95fbb464
Sauma Saha <saumasaha@Saumas-MacBook-Pro.local>
2023-04-11 16:51:49 +0530

    Upgraded the machine to determine how many 1 and 2 rupees coin

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,8 @@
-const  coinsDispensed = function (rupees) {
-  return rupees;
+const  coinsToDispense = function (rupees) {
+  const twoRupeesCoins = Math.floor(rupees / 2);
+  const remainingAmount = rupees % 2;
+  const oneRupeeCoin = remainingAmount / 1;
+  return  twoRupeesCoins + oneRupeeCoin;
 }
 
-exports.coinsDispensed = coinsDispensed;
+exports.coinsToDispense = coinsToDispense;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -3,9 +3,15 @@ const testing = require('../lib/testing.js');
 
 const assert = testing.assertTest;
 const displayTestLog = testing.displayTestLog;
-const coinsDispensed = vendingMachine.coinsDispensed;
+const coinsToDispense = vendingMachine.coinsToDispense;
 
-assert(coinsDispensed(0), 0, "0 rupees should return 0 coins", "coinsDispensed()");
-assert(coinsDispensed(1), 1, "1 rupees should return 0 coins", "coinsDispensed()");
+const testCoinsToDispense = function () {
+  assert(coinsToDispense(0), 0, "0 rupees should return 0 coins", "coinsToDispense()");
+  assert(coinsToDispense(1), 1, "1 rupees should return 1 coins", "coinsToDispense()");
+  assert(coinsToDispense(2), 1, "2 rupees should return 1 coins", "coinsToDispense()");
+  assert(coinsToDispense(3), 2, "3 rupees should return 2 coins", "coinsToDispense()");
+  assert(coinsToDispense(5), 3, "5 rupees should return 3 coins", "coinsToDispense()");
+}
 
+testCoinsToDispense();
 displayTestLog();
