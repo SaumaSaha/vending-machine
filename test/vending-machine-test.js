f5b0f7619409f62eb9695592d9136dbbdeddb91a
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-04-12 20:00:40 +0530

    Added feature of dispensing coins with denominations given in any order

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -24,8 +24,17 @@ const testCoinsToDispense = function () {
   assert(4, dispenseCoins(18, [1, 5, 10, 2,]), "18 rupees with [1, 5, 10, 2] denominations should give 4 coins", "dispenseCoins()");
 }
 
-assertArray([10, 5, 2, 1], maxSort([1, 5, 10, 2]), "[1, 5, 10, 2] should give a sorted array in decending order [10, 5, 2, 1]", "maxSort()");
-assert(10, maxOfNumbers([1, 5, 10, 2]), "[1, 5, 10, 2] should give the max number 10", "maxOfNumbers()");
-assert(20, maxOfNumbers([1, 5, 10, 20, 7, 6, 2]), "[1, 5, 10, 20, 7, 6, 2] should give the max number 20", "maxOfNumbers()");
-// testCoinsToDispense();
+const testMaxOFNumbers = function () {
+  assert(10, maxOfNumbers([1, 5, 10, 2]), "[1, 5, 10, 2] should give the max number 10", "maxOfNumbers()");
+  assert(20, maxOfNumbers([1, 5, 10, 20, 7, 6, 2]), "[1, 5, 10, 20, 7, 6, 2] should give the max number 20", "maxOfNumbers()");
+}
+
+const testMaxSort = function () {
+  assertArray([10, 5, 2, 1], maxSort([1, 5, 10, 2]), "[1, 5, 10, 2] should give a sorted array in decending order [10, 5, 2, 1]", "maxSort()");
+  assertArray([20, 10, 9, 5, 2, 1], maxSort([1, 20, 5, 9, 10, 2]), "[1, 5, 10, 2] should give a sorted array in decending order [10, 5, 2, 1]", "maxSort()");
+}
+
+testCoinsToDispense();
+testMaxOFNumbers();
+testMaxOFNumbers();
 displaySummary();
