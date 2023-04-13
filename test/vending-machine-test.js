997f5abf644b836abed77ec2b25f678bf798ecd0
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-04-14 00:28:26 +0530

    Refactored test cases and made a style.js utility that styles text

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -12,41 +12,50 @@ const maxSort = vendingMachine.maxSort;
 const maxOfNumbers = vendingMachine.maxOfNumbers;
 const getCoinsByAmount = vendingMachine.getCoinsByAmount;
 
-const testCoinsToDispense = function () {
-  displayHeader("coinsToDispense");
-  assert(0, dispenseCoins(0, []), "0 rupees with [] denominations should return 0 coins", "dispenseCoins()");
-  assert(1, dispenseCoins(1, [1]), "1 rupees with [1] denominations should return 1 coins", "dispenseCoins()");
-
-  assert(1, dispenseCoins(2, [1, 2]), "2 rupees with [1, 2] denominations should return 1 coins", "dispenseCoins()");
-  assert(2, dispenseCoins(3, [1, 2]), "3 rupees with [1, 2] denominations should return 2 coins", "dispenseCoins()");
-
-  assert(1, dispenseCoins(5, [1, 2, 5]), "5 rupees with [1, 2, 5] denominations should return 1 coins", "dispenseCoins()");
-
-  assert(1, dispenseCoins(10, [1, 2, 5, 10]), "10 rupees with [1, 2, 5, 10] denominations should return 1 coins", "dispenseCoins()");
-  assert(3, dispenseCoins(8, [1, 2, 5, 10]), "8 rupees with [1, 2, 5, 10] denominations should return 3 coins", "dispenseCoins()");
-  assert(2, dispenseCoins(12, [1, 2, 5, 10]), "12 rupees with [1, 2, 5, 10] denominations should return 2 coins", "dispenseCoins()");
-  assert(4, dispenseCoins(18, [1, 2, 5, 10]), "18 rupees with [1, 2, 5, 10] denominations should return 4 coins", "dispenseCoins()");
-
-  assert(4, dispenseCoins(13, [1, 2, 5]), "13 rupees with [1, 2, 5] denominations should give 4 coins", "dispenseCoins()");
-  assert(4, dispenseCoins(13, [1, 4, 7]), "13 rupees with [1, 4, 7] denominations should give 4 coins", "dispenseCoins()");
-  assert(4, dispenseCoins(18, [1, 5, 10, 2,]), "18 rupees with [1, 5, 10, 2] denominations should give 4 coins", "dispenseCoins()");
+const testOrderedDenominations = function (fnName) {
+  let message = "₹0 gives gives nothing";
+  assert(0, dispenseCoins(0, []), message , fnName);
+  message = "₹1 vending machine gives the same amount of coins as the money";
+  assert(1, dispenseCoins(1, [1]), message, fnName);
+  message = "₹1 and ₹2 coin vending machine when given amount ₹2 gives 1 coin of ₹2";
+  assert(1, dispenseCoins(2, [1, 2]), message, fnName);
+  message = "₹1 and ₹2 coin vending machine when given amount ₹3 gives 2 coins one coin of each denomination";
+  assert(2, dispenseCoins(3, [1, 2]), message, fnName);
+  message = "₹1, ₹2 and ₹5 coin vending machine when given amount ₹5 gives 1 coins of ₹5" ;
+  assert(1, dispenseCoins(5, [1, 2, 5]), message, fnName);
+  message = "₹1, ₹2, ₹5 and ₹10 coin vending machine when given amount ₹10 gives 1 coins of ₹10" ;
+  assert(1, dispenseCoins(10, [1, 2, 5, 10]), message, fnName);
+  message = "₹1, ₹2, ₹5 and ₹10 coin vending machine when given amount ₹18 gives 4 coins one coin of each denomination" ;
+  assert(4, dispenseCoins(18, [1, 2, 5, 10]), message, fnName);
 }
-/* function testMaxOfNumbers ---------------{{{ */
+
+const testCoinsToDispense = function () {
+  const fnName = "dispenseCoins";
+  displayHeader(fnName);
+  testOrderedDenominations(fnName);
+  let message = "Testing with sorted denominations";
+  assert(4, dispenseCoins(13, [1, 2, 5]), message, fnName);
+  message =  "Testing with any denominations";
+  assert(4, dispenseCoins(13, [1, 4, 7]), message, fnName);
+  message = "Testing with unsorted denominations";
+  assert(4, dispenseCoins(18, [1, 5, 10, 2,]), message, fnName);
+}
+
 const testMaxOFNumbers = function () {
   displayHeader("maxOfNumbers");
   assert(10, maxOfNumbers([1, 5, 10, 2]), "[1, 5, 10, 2] should give the max number 10", "maxOfNumbers()");
   assert(20, maxOfNumbers([1, 5, 10, 20, 7, 6, 2]), "[1, 5, 10, 20, 7, 6, 2] should give the max number 20", "maxOfNumbers()");
 }
-/*----}}} */
+
 const testMaxSort = function () {
-  const fnName = ""
+  const fnName = "maxSort";
   displayHeader(fnName);
   assertArray([10, 5, 2, 1], maxSort([1, 5, 10, 2]), "[1, 5, 10, 2] should give a sorted array in decending order [10, 5, 2, 1]", "maxSort()");
   assertArray([20, 10, 9, 5, 2, 1], maxSort([1, 20, 5, 9, 10, 2]), "[1, 5, 10, 2] should give a sorted array in decending order [10, 5, 2, 1]", "maxSort()");
 }
 
-const testDispenseCoinsByDenominations =  function () {
-  const fnName = "getCoinsByAmount"
+const testGetCoinsByAmount =  function () {
+  const fnName = "getCoinsByAmount";
   displayHeader(fnName);
   assertObject(
     {1:1, 2:1,},
@@ -63,5 +72,5 @@ const testDispenseCoinsByDenominations =  function () {
 testCoinsToDispense();
 testMaxOFNumbers();
 testMaxSort();
-testDispenseCoinsByDenominations();
+testGetCoinsByAmount();
 displaySummary();
